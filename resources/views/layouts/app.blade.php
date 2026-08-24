32e14583c9c5468456675d09287555ffea842159
@CML-ebedoya <ebedoya@cmlexports.com>
2026-08-24 17:26:40 -0400

    Replace default Laravel red favicon with official Pymora SVG logo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'Pymora - Sistema Administrativo & Financiero SaaS')</title>
+    <!-- Favicon Pymora Emblem Logo -->
+    <link rel="icon" type="image/svg+xml" href="{{ asset('favicon.svg') }}">
+    <link rel="shortcut icon" href="{{ asset('favicon.svg') }}">
     <!-- Google Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
